0a5f3d1c16c387d15e56c92eb06d7e72e430f125
LammaSF <lilibabacheva@yahoo.com>
2017-10-02 03:09:40 +0300

    gitignor and firebase

---
+++ app/config/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyDOW6_PHj3qSv6tHAQU6EZYsocWD67ODbU',
+    authDomain: 'mycuisine-42c85.firebaseapp.com',
+    databaseURL: 'https://mycuisine-42c85.firebaseio.com',
+    projectId: 'mycuisine-42c85',
+    storageBucket: 'mycuisine-42c85.appspot.com',
+    messagingSenderId: '752180555366'
+};
