1d11cb51ff1066af96459540a2e34e7b45ae5d63
LammaSF <lilibabacheva@yahoo.com>
2017-10-07 22:16:13 +0300

    my carousel works autoplay

--- public/controllers/homeController.js
+++ public/controllers/homeController.js
@@ -4,8 +4,28 @@ import 'jquery';
 
 
 function all(){
+
     $('.common-footer').css("display","none");
-        templates.getPage('home');
+        templates.getPage('home', {})
+        .done(() => {
+            jQuery(document).ready(function($) {
+
+                $('#myCarousel').carousel({
+                    interval: 5000
+                });
+            })
+            //Handles the carousel thumbnails
+            $('[id^=carousel-selector-]').click(function () {
+                var id_selector = $(this).attr("id");
+                var id = /-(\d+)$/.exec(id_selector)[1];
+                jQuery('#myCarousel').carousel(parseInt(id));
+            });
+            // When the carousel slides, auto update the text
+            $('#myCarousel').on('slid.bs.carousel', function (e) {
+                var id = $('.item.active').data('slide-number');
+                $('#carousel-text').html($('#slide-content-'+id).html());
+            });
+    })
 }
 
 
