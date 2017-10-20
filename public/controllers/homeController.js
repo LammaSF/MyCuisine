eea45bb850f9ef42e6f9bb2e5d39dbe129e18e85
LammaSF <lilibabacheva@yahoo.com>
2017-10-21 01:53:35 +0300

    slider bootstrap

--- public/controllers/homeController.js
+++ public/controllers/homeController.js
@@ -3,30 +3,53 @@ import 'bootstrap';
 import 'jquery';
 
 
-function all(){
+function all() {
 
-    $('.common-footer').css("display","none");
-        templates.getPage('home', {})
-        .done(() => {
-            jQuery(document).ready(function($) {
+    $('.common-footer').css("display", "none");
+    return new Promise((resolve, reject) => {
+        let cat = "tarts";
+        let reviews = firebase.database().ref('menu/tarts');
+        let $this = $(this);
 
-                $('#myCarousel').carousel({
-                    interval: 5000,
-                });
+        reviews.once('value', (snapshot) => {
+            let f = [];
+
+            snapshot.forEach(element => {
+
+                let dbElements = {
+                    'description': element.val().description,
+                    'price': element.val().price,
+                    'image': element.val().image,
+                    'name': element.key,
+                    'category': cat
+
+                };
+                f.push(dbElements);
+
 
             })
-            //Handles the carousel thumbnails
-            $('[id^=carousel-selector-]').click(function () {
-                let id_selector = $(this).attr("id");
-                let id = /-(\d+)$/.exec(id_selector)[1];
-                jQuery('#myCarousel').carousel(parseInt(id));
-            });
-            // When the carousel slides, auto update the text
-            $('#myCarousel').on('slid.bs.carousel', function (e) {
-                let id = $('.item.active').data('slide-number');
+            templates.getPage('home', f) .done(() => {
+                jQuery(document).ready(function ($) {
 
-                $('#carousel-text').html($('#slide-content-'+id).html());
-            });
+                    $('#myCarousel').carousel({
+                        interval: 7000,
+                    });
+
+                })
+                //Handles the carousel thumbnails
+                $('[id^=carousel-selector-]').click(function () {
+                    let id_selector = $(this).attr("id");
+                    let id = /-(\d+)$/.exec(id_selector)[1];
+                    jQuery('#myCarousel').carousel(parseInt(id));
+                });
+                // When the carousel slides, auto update the text
+                $('#myCarousel').on('slid.bs.carousel', function (e) {
+                    let id = $('.item.active').data('slide-number');
+
+                    $('#carousel-text').html($('#slide-content-' + id).html());
+                });
+            })
+        })
     })
 }
 
@@ -37,4 +60,4 @@ let homeControl = {
     all,
 }
 
-export { homeControl as homeController }
+export { homeControl as homeController };
